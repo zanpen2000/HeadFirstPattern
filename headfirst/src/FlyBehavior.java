1c9dc0068eedb3ba227be75cdca3890db96a97bb
zanpen2000 <zanpen2000@gmail.com>
2013-06-28 18:54:08 +0800

    Head First设计模式代码

---
+++ headfirst/src/FlyBehavior.java
@@ -0,0 +1,6 @@
+
+public interface FlyBehavior {
+
+	void fly();
+
+}
---
+++ headfirst/src/MallardDuck.java
@@ -0,0 +1,16 @@
+
+public class MallardDuck extends Duck {
+
+	public MallardDuck(){
+		this.flyBehavior  = new FlyWithWings();
+		this.quackBehavior = new Quack();
+		
+	}
+	
+	@Override
+	public void display() {
+		System.out.println("I'm a MallardDuck");
+	}
+
+	
+}
